d5ce40f728dd11a4f23937fa2ba686e52f80e1de
wsameer <dev.wsameer@gmail.com>
2021-08-29 14:34:34 +0530

    build: installs more config dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,99 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
-const SRC = path.resolve(__dirname, "../../src");
-const DIST = path.resolve(__dirname, "../../dist");
-const ENTRY = path.resolve(__dirname, "../../src/index");
-const STATIC = path.resolve(__dirname, "../../public/static");
-const INDEX_HTML = path.resolve(__dirname, "../../public/index.html");
+const PACKAGE_ROOT_PATH = process.cwd(),
+    SRC = path.resolve(PACKAGE_ROOT_PATH, './src'),
+    DIST = path.resolve(PACKAGE_ROOT_PATH, './dist'),
+    PUBLIC = path.resolve(PACKAGE_ROOT_PATH, './public'),
+    INDEX_HTML = path.resolve(PACKAGE_ROOT_PATH, './public/index.html');
 
 module.exports = {
-    entry: [ENTRY],
     output: {
         path: DIST,
-        filename: "[name].[hash].js",
-        chunkFilename: "[name].[hash].js",
-        publicPath: "/",
+        filename: '[name].[contenthash].js',
+        chunkFilename: '[name].[contenthash].js',
+        publicPath: '/',
     },
+    performance: {
+        maxAssetSize: 500000,
+        maxEntrypointSize: 500000,
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx', '.json'],
+        symlinks: false,
+        plugins: [new TsconfigPathsPlugin()],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: '@svgr/webpack',
+                        options: {
+                            icon: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.tsx?$/,
+                include: SRC,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'thread-loader',
+                        options: {
+                            workers: 2,
+                            workerParallelJobs: 50,
+                        },
+                    },
+                    'babel-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: INDEX_HTML,
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+            },
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: PUBLIC,
+                    to: DIST,
+                    globOptions: {
+                        ignore: ['**/index.html'],
+                    },
+                },
+            ],
+        }),
+        new ForkTsCheckerWebpackPlugin(),
+        new CircularDependencyPlugin({
+            exclude: /node_modules/,
+            failOnError: true,
+        }),
+        new CompressionPlugin(),
+        new Dotenv({ systemvars: true }),
+    ],
 };
