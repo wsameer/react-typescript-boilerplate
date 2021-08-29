0575af32b0803b6ce23c1b2bcbf145b593159b34
wsameer <dev.wsameer@gmail.com>
2021-08-29 10:08:12 +0530

    feat: adds src folder and App component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome!</h1>
+            <p>Edit App.tsx to create something awesome!</p>
+        </div>
+    );
+};
+
+export default App;
