156659e2ea1f9c89097ec7d93c1db7156f8c9f63
wsameer <dev.wsameer@gmail.com>
2021-08-30 18:47:21 +0530

    test(App): adds tests

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('<App />', () => {
+    render(<App />);
+    expect(screen.getByText(/Welcome/i)).toBeInTheDocument();
+});
