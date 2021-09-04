3d5666dd5153ab6d9f55bcb9b58b67199d8f99ac
wsameer <dev.wsameer@gmail.com>
2021-09-04 10:26:28 +0530

    build(babel): adds style plugins

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = function (api) {
-    const presets = ["@babel/preset-env", "@babel/react", "@babel/typescript"],
+    const presets = ['@babel/preset-env', '@babel/react', '@babel/typescript'],
         plugins = [
-            "@babel/plugin-transform-runtime",
+            '@babel/plugin-transform-runtime',
             [
-                "inline-react-svg",
+                'inline-react-svg',
                 {
                     svgo: {
                         plugins: [
@@ -18,8 +18,8 @@ module.exports = function (api) {
             ],
         ];
 
-    if (api.env() === "development") {
-        plugins.push("react-hot-loader/babel");
+    if (api.env() === 'development') {
+        plugins.push('react-hot-loader/babel');
     }
 
     return { presets, plugins };
